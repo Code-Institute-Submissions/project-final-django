12166bd628c12dbfa7122fa9d8d3387cc8288a15
SDV <steindevos@gmail.com>
2018-08-22 15:31:45 +0000

    created env variable of secret key

--- webshop/settings.py
+++ webshop/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(g^&2!+2wskat+6zz=%k&iw*p!s9_*nr(%*be(m93=qz&g)bjz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-final-django-steindevos.c9users.io']
 
 
 # Application definition
