fd37787ca0157ae9a917c690c2b85104d15362d1
SDV <steindevos@gmail.com>
2018-08-22 16:42:35 +0000

    added a product model and functionality to show products on products-list page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def products_list(request):
-    return render(request, "products/show_products.html")
+    products = Product.objects.all()
+    return render(request, "products/show_products.html", {'products': products})
