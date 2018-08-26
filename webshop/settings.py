82086993c2c4fff7ae4389090145c90b6f0729f1
SDV <steindevos@gmail.com>
2018-08-26 17:16:59 +0000

    added shopping cart count function

--- webshop/settings.py
+++ webshop/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.number_of_items_in_cart',
             ],
         },
     },
